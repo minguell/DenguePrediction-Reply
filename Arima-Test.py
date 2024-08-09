887a51894e96f0d3f2642d80dad94196c35490fc
Miguel Dutra <127598214+minguell@users.noreply.github.com>
2024-08-09 14:52:49 -0300

    Update Arima-Test.py

--- Arima-Test.py
+++ Arima-Test.py
@@ -1,10 +1,10 @@
 import pandas as pd
 import numpy as np
-from statsmodels.tsa.arima.model import ARIMA
+from statsmodels.tsa.statespace.sarimax import SARIMAX
 from sklearn.preprocessing import MinMaxScaler
 
 # Passo 1: Leitura do CSV
-file_path = 'path_to_your_file.csv'  # Substitua pelo caminho do seu arquivo
+file_path = './data/dengue_input_simple.csv'  # Substitua pelo caminho do seu arquivo
 data = pd.read_csv(file_path, delimiter=';')
 
 # Passo 2: Seleção e normalização das features
@@ -19,21 +19,22 @@ data = data[features]
 scaler = MinMaxScaler(feature_range=(0, 1))
 scaled_data = scaler.fit_transform(data)
 
-# Passo 3: Preparação dos dados para o ARIMA
-# Para o ARIMA, usaremos apenas a coluna target 'dengue_diagnosis'
+# Passo 3: Preparação dos dados para o SARIMA
+# Para o SARIMA, usaremos apenas a coluna target 'dengue_diagnosis'
 dengue_diagnosis = scaled_data[:, 0]
 
-# Passo 4: Ajuste do modelo ARIMA
-# Escolha dos parâmetros p, d, q (esses parâmetros podem ser ajustados conforme necessário)
-p, d, q = 5, 1, 0  # Você pode ajustar esses valores conforme necessário
+# Passo 4: Ajuste do modelo SARIMA
+# Escolha dos parâmetros p, d, q, P, D, Q, m (esses parâmetros podem ser ajustados conforme necessário)
+p, d, q = 1, 1, 1  # Parâmetros ARIMA
+P, D, Q, m = 1, 1, 1, 12  # Parâmetros sazonais (m = 12 para sazonalidade anual)
 
-# Ajuste do modelo ARIMA
-model = ARIMA(dengue_diagnosis, order=(p, d, q))
-model_fit = model.fit()
+# Ajuste do modelo SARIMA
+model = SARIMAX(dengue_diagnosis, order=(p, d, q), seasonal_order=(P, D, Q, m))
+model_fit = model.fit(disp=False)
 
 # Passo 5: Fazendo previsões
 # Previsão para o próximo período
 forecast = model_fit.forecast(steps=1)
 forecast = scaler.inverse_transform(np.concatenate([forecast.reshape(-1, 1), np.zeros((forecast.shape[0], scaled_data.shape[1] - 1))], axis=1))[:, 0]
 
-print("Previsão do nível de infestação de dengue para o próximo dia:", forecast[0])
+print("Previsão do nível de infestação de dengue para o próximo mes:", forecast[0])
